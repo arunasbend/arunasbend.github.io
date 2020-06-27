7d873f83ec69be23950cc3ce37189cc7fb5ad1b5
ArunasB <baronas15@gmail.com>
2020-06-27 14:20:04 +0300

    Remove build from git

--- build/js/custom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  /*------------------------
-	Page Loader
-	--------------------------*/
-  jQuery("#load").fadeOut();
-  jQuery("#loading").delay(0).fadeOut("slow");
-});
